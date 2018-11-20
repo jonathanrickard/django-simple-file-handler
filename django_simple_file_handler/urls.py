c587d7f0c7b0464f34bd17765bfb70be0d9d8e70
jonathanrickard <jonathan.rickard@gmail.com>
2018-11-20 17:05:21 -0500

    Changed tabs to spaces

--- django_simple_file_handler/urls.py
+++ django_simple_file_handler/urls.py
@@ -10,14 +10,14 @@ app_name = 'django_simple_file_handler'
 
 
 urlpatterns = [
-	url(
-	    r'^documents/(?P<proxy_slug>.*)',
-	    proxy_document,
-	    name='proxy_document',
-	),
-	url(
-	    r'^pdf/(?P<proxy_slug>.*)',
-	    proxy_pdf,
-	    name='proxy_pdf',
-	),
+    url(
+        r'^documents/(?P<proxy_slug>.*)',
+        proxy_document,
+        name='proxy_document',
+    ),
+    url(
+        r'^pdf/(?P<proxy_slug>.*)',
+        proxy_pdf,
+        name='proxy_pdf',
+    ),
 ]
